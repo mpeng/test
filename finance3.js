441cb09f92866b602939526ffab1fce2aa2b91e6
mpeng <wensheng.peng@gmail.com>
2017-02-17 19:34:32 -0500

    Small change

--- finance3.js
+++ finance3.js
@@ -3,7 +3,7 @@
 angular.module('finance3', [])
   .factory('currencyConverter', ['$http', function($http) {
     var YAHOO_FINANCE_URL_PATTERN =
-          '//query.yahooapis.com/v1/public/yql?q=select * from ' +
+          'http://query.yahooapis.com/v1/public/yql?q=select * from ' +
           'yahoo.finance.xchange where pair in ("PAIRS")&format=json&' +
           'env=store://datatables.org/alltableswithkeys';
     var currencies = ['USD', 'EUR', 'CNY'];
@@ -13,16 +13,23 @@ angular.module('finance3', [])
       return amount * usdToForeignRates[outCurr] / usdToForeignRates[inCurr];
     };
 
+    console.log("Here0");
     var refresh = function() {
+      console.log("Here1a");
       var url = YAHOO_FINANCE_URL_PATTERN.
                  replace('PAIRS', 'USD' + currencies.join('","USD'));
+      console.log("Here1b");
       return $http.get(url).then(function(response) {
         var newUsdToForeignRates = {};
+        console.log("Here1c");
         angular.forEach(response.data.query.results.rate, function(rate) {
           var currency = rate.id.substring(3,6);
+          console.log("Here2=" + currency);
           newUsdToForeignRates[currency] = window.parseFloat(rate.Rate);
+          console.log("Here3=" + newUsdToForeignRates[currency]);
         });
         usdToForeignRates = newUsdToForeignRates;
+        console.log("Here4=" + usdToForeignRates);
       });
     };
 
